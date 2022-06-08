ccbb7372f79e5651744dccbf042c9bb20ac1c903
c0w3rd <107107662+c0w3rd@users.noreply.github.com>
2022-06-08 02:07:41 -0700

    Update afk interrobang.py

--- afk interrobang.py
+++ afk interrobang.py
@@ -1,11 +1,11 @@
-import pynput
-from pynput.mouse import Controller, Button
+from pynput.mouse import Controller
 import random
 mouse = Controller()
 mposX, mposY = mouse.position
 amount = int(input("How much movement? (px, use whole number):"))
+
 while True:
     mposX, mposY = mouse.position
-    mposmoveX = random.uniform((0.0 - float(mposX+amount)), (mposX+amount))
-    mposmoveY = random.uniform((0.0 - float(mposY+amount)), (mposY+amount))
+    mposmoveX = random.randint((0 - amount) + mposX, amount + mposX)
+    mposmoveY = random.randint((0 - amount) + mposY, amount + mposY)
     mouse.position = (mposmoveX, mposmoveY)
