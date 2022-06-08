8bb1ad9681ac6d0306d16569268dafe64f02be9b
c0w3rd <107107662+c0w3rd@users.noreply.github.com>
2022-06-08 02:11:29 -0700

    Update afk interrobang.py

--- afk interrobang.py
+++ afk interrobang.py
@@ -1,11 +1,25 @@
+#Imports
 from pynput.mouse import Controller
 import random
+
+#Initial setup and variables to start with so mouse isn't totally fucked
 mouse = Controller()
 mposX, mposY = mouse.position
+
+#Ask user how much the shit should move, really funny when they say like 100 or smth
 amount = int(input("How much movement? (px, use whole number):"))
 
+#funnee loop
 while True:
+
+    #getting the mouse position as variables every iteration so that it actually, yk, moves
     mposX, mposY = mouse.position
+
+    #Getting the places to move the mouse - each cooridnate moves a random amount between the negative amount that we asked for plus the current position to the amount plus move position
     mposmoveX = random.randint((0 - amount) + mposX, amount + mposX)
     mposmoveY = random.randint((0 - amount) + mposY, amount + mposY)
+
+    #Actually moves the mouse
     mouse.position = (mposmoveX, mposmoveY)
+
+#BE CAREFUL - there is no failsafe on this so use a text editor or compiler that has a command built in
